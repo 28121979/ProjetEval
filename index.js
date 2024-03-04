a5787fe8f7d8b85ceb7a1618e78534dfd6bfeb20
camara <ninobrowns028@gmail.com>
2024-03-04 09:56:53 +0100

    ajout d'éléments

--- index.js
+++ index.js
@@ -1,68 +1,55 @@
-// let meubleData = []
 
-// fetch('http://localhost:3000/api/furniture')
-//     .then(response => response.json())
-//     .then(data => {
-//         meubleData = data
-//         console.log(meubleData)
-//     })
-
-//     const meubleDisplay = async () => {
-//        await fetchMeuble() 
-//     }
+document.addEventListener('DOMContentLoaded', () => {
+    let meubleData = [];
     
-//    document.getElementById('blocCard').innerHTML = meubleData.map((meuble) => `
-//    <div id="card${meuble._id}" class="card col-12 col-md-6 col-lg-4 col-xl-3 my-3 mx-3">
-//    <h3 class="titre-card">${meuble.name}</h3>
-//    `)
-
-//    meubleDisplay()
-
-let meubleData = [];
-
-// Fonction pour récupérer les données et mettre à jour l'interface utilisateur
-const fetchMeublesAndUpdateUI = async () => {
-    try {
-        const response = await fetch('http://localhost:3000/api/furniture');
-        if (!response.ok) {
-            throw new Error('Erreur réseau');
+    // Fonction pour récupérer les données des meubles et mettre à jour l'interface utilisateur
+    const fetchMeublesAndUpdateUI = async () => {
+        try {
+            const response = await fetch('http://localhost:3000/api/furniture');
+            if (!response.ok) {
+                throw new Error('Erreur réseau');
+            }
+            meubleData = await response.json();
+            updateUI();
+            updateCartQuantityUI();
+        } catch (error) {
+            console.error("Erreur lors de la récupération des meubles:", error);
         }
-        meubleData = await response.json();
-        updateUI(); // Appel de la fonction pour mettre à jour l'UI une fois les données récupérées
-    } catch (error) {
-        console.error("Erreur lors de la récupération des meubles:", error);
-    }
-};
-
-// Fonction pour mettre à jour l'interface utilisateur
-const updateUI = () => {
-    const blocCard = document.getElementById('blocCard');
-    if (blocCard) {
-        blocCard.innerHTML = meubleData.map((meuble) =>
-            `<div id="card${meuble._id}" class="flex-around card col-12 col-md-6 col-lg-4 col-xl-3 my-3 mx-3">
-                <h3 class="titre-card">${meuble.name .toUpperCase()}</h3>
-                <img class="img-fluid" src="${meuble.imageUrl}" alt="photo de meuble ${meuble.name}"/>
-                <div class="bouton-chene">CHÊNE</div>
-                <p class="description-meuble">${meuble.description}</p>
-                <button id="${meuble._id}" class="bouton-details">Voir</button>
-                 <p>${(meuble.price / 100).toFixed(2).replace(/\.?0+$/, '')}€</p>
-
-
-             </div>`
-        ).join(''); // Utilisez join('') pour transformer le tableau en chaîne de caractères
-
-        let boutons = document.querySelectorAll('.bouton-details');
-        console.log(boutons);
-
-        boutons.forEach(bouton => {
-            bouton.addEventListener('click', () => {
-               
-
-                window.location.href = `produit.html?id=${bouton.id}`
-            })
-        })
-    }
-};
-
-// Appel de la fonction pour démarrer le processus
-fetchMeublesAndUpdateUI();
+    };
+    
+    // Fonction pour mettre à jour l'interface utilisateur avec les données des meubles
+    const updateUI = () => {
+        const blocCard = document.getElementById('blocCard');
+        if (blocCard) {
+            blocCard.innerHTML = meubleData.map(meuble =>
+                `<div id="card${meuble._id}" class="flex-around card col-12 col-md-6 col-lg-4 col-xl-3 my-3 mx-3">
+                    <h3 class="titre-card">${meuble.name.toUpperCase()}</h3>
+                    <img class="img-fluid" src="${meuble.imageUrl}" alt="photo de meuble ${meuble.name}"/>
+                    <div class="bouton-chene">CHÊNE</div>
+                    <p class="description-meuble">${meuble.description}</p>
+                    <button id="${meuble._id}" class="bouton-details">Voir</button>
+                    <p>${(meuble.price / 100).toFixed(2).replace(/\.?0+$/, '')}€</p>
+                 </div>`
+            ).join('');
+    
+            document.querySelectorAll('.bouton-details').forEach(bouton => {
+                bouton.addEventListener('click', () => {
+                    window.location.href = `produit.html?id=${bouton.id}`;
+                });
+            });
+        }
+    };
+    
+    // Fonction pour mettre à jour l'affichage de la quantité totale des produits dans le panier
+    const updateCartQuantityUI = () => {
+        const produits = JSON.parse(localStorage.getItem('produit')) || [];
+        const totalQuantite = produits.reduce((total, produit) => total + produit.quantite, 0);
+    
+        const cartQuantityElement = document.getElementById('cartQuantity');
+        if (cartQuantityElement) {
+            cartQuantityElement.textContent = totalQuantite;
+        }
+    };
+    
+    fetchMeublesAndUpdateUI();
+});
--- produit.js
+++ produit.js
@@ -1,225 +1,147 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const produitId = urlParams.get('id');
 
-// // Fonction pour récupérer les données du produit
-// async function fetchProduit() {
-//     if (!produitId) {
-//         console.log('ID du produit non spécifié dans l\'URL');
-//         return;
-//     }
-//     try {
-//         const response = await fetch(`http://localhost:3000/api/furniture/${produitId}`);
-//         if (!response.ok) {
-//             throw new Error('La réponse du réseau n\'était pas OK');
-//         }
-//         const produitData = await response.json();
-//         displayProduit(produitData);
-//     } catch (error) {
-//         console.error('Erreur lors de la récupération du produit:', error);
-//     }
-// }
-
-// // Fonction pour afficher les données du produit
-// function displayProduit(produitData) {
-//     const blocCard = document.getElementById('blocCard');
-//     if (!blocCard) {
-//         console.error('Element #blocCard non trouvé dans le DOM');
-//         return;
-//     }
-
-//     let optionsVernis = produitData.varnish.map(vernis => `<option value="${vernis}">${vernis}</option>`).join('');
-//     blocCard.innerHTML = `
-//         <div id="card${produitData._id}" class="card">
-//             <h3>${produitData.name.toUpperCase()}</h3>
-//             <img src="${produitData.imageUrl}" alt="photo de meuble ${produitData.name}" class="image-size"/>
-//             <p>${produitData.description}</p>
-//             <p>Prix: ${produitData.price / 100} €</p>
-//             <div class="flex-around div-list-deroulante">
-//                 <label for="vernis">Choisir le vernis:</label>
-//                 <select name="vernis" id="vernis">${optionsVernis}</select>
-//             </div>
-//             <button id="btnAcheter" class="bouton-details bouton-acheter">Acheter</button>
-//         </div>
-//     `;
-//     addBasket();
-// }
-
-// // Ajoute le produit au panier
-// function addBasket() {
-//     const bouton = document.getElementById('btnAcheter');
-//     if (!bouton) return;
-
-//     bouton.addEventListener('click', () => {
-//         let produitTableau = JSON.parse(localStorage.getItem('produit')) || [];
-//         let select = document.getElementById('vernis');
-//         const produitAajouter = {
-//             id: produitId,
-//             teinte: select.value,
-//             quantite: 1
-//         };
-
-//         const indexProduit = produitTableau.findIndex(produit => produit.id === produitAajouter.id && produit.teinte === produitAajouter.teinte);
-//         if(indexProduit !== -1) {
-//             produitTableau[indexProduit].quantite += 1;
-//         } else {
-//             produitTableau.push(produitAajouter);
-//         }
-
-//         localStorage.setItem('produit', JSON.stringify(produitTableau));
-//     });
-// }
-
-// fetchProduit(); // Exécution de la fonction principale pour récupérer et afficher les données du produit
-
-// const produit = window.location.search.split("?").join("")
-
-// console.log(produit);
-
-
-// let produitData = []
+// let produitData = {};
 
 // const fetchProduit = async () => {
-//     await fetch(`http://localhost:3000/api/furniture/${produit}`).then((res)=> 
-//     res.json()
-//     ).then((data)=>{
-//         console.log(data);
-//     })
-// }
-
-// fetchProduit()
-
-
-
-
-
-
-
-// let produitData = [];
-// const fetchProduit = async () => {
-//     // Extraction de l'ID du produit depuis l'URL de la page
 //     const produitId = window.location.search.split("=")[1];
-
 //     try {
 //         const response = await fetch(`http://localhost:3000/api/furniture/${produitId}`);
-//         const data = await response.json();
-//         console.log(data);
+//         produitData = await response.json();
+//         console.log("Produit récupéré:", produitData);
 //     } catch (error) {
 //         console.error('Erreur lors de la récupération du produit:', error);
-
-//         produitData = data
-//         console.log(produitData);
 //     }
 // };
 
 // const produitDisplay = async () => {
 //     await fetchProduit();
+//     if (Object.keys(produitData).length === 0) {
+//         console.error('Aucun produit trouvé');
+//         return;
+//     }
    
 //     document.getElementById('blocCard').innerHTML = `
-//     <div id="card${produitData._id}" class="flex-column-around card-size-produit card">
-//     <h3 class="titre-card titre-size">${produit.Datta.name .toUppercase()}</h3>
-//     <img class="image-size" src="${produitData.imageUrl}" alt="image de meuble ${produitData.name}">
-//     <div class="bouton-chene">CHÊNE</div>
-//     <p class="description-meuble">${produitData.description}</p>
-//     <div>
-//     <p class="flex-centre"> ${produitData.price .toString().replace(/0+$/,"")} Euro</p>
-    
-//     <div class="flex-around div-list-deroulante">
-//     <label for="vernis">Choisir le vernis:</label>
-//     <select name="vernis" id="vernis"></select>
-//     </div>
-//     </div>
-//     <button id="${produitData._id}" class="bouton-details bouton-acheter">Acheter</button>
-//     </div>
-// `;
+//         <div id="card${produitData._id}" class="flex-column-around card-size-produit card">
+//             <h3 class="titre-card titre-size">${produitData.name.toUpperCase()}</h3>
+//             <img class="image-size" src="${produitData.imageUrl}" alt="image de meuble ${produitData.name}">
+//             <div class="bouton-chene">CHÊNE</div>
+//             <p class="description-meuble">${produitData.description}</p>
+//             <div>
+//                 <p class="flex-centre">${(produitData.price / 100).toFixed(2)} Euro</p>
+//                 <div class="flex-around div-list-deroulante">
+//                     <label for="vernis">Choisir le vernis:</label>
+//                     <select name="vernis" id="vernis"></select>
+//                 </div>
+//             </div>
+//             <button id="acheter${produitData._id}" class="bouton-details bouton-acheter">Acheter</button>
+//         </div>
+//     `;
 
-    
-// }
+//     const select = document.getElementById('vernis');
+//     produitData.varnish.forEach(vernis => {
+//         const tagOption = document.createElement('option');
+//         tagOption.textContent = vernis;
+//         tagOption.value = vernis;
+//         select.appendChild(tagOption);
+//     });
+
+//     addBasket();
+// };
 
 // produitDisplay();
 
+// const addBasket = () => {
+//     const bouton = document.getElementById(`acheter${produitData._id}`);
+//     bouton.addEventListener('click', () => {
+//         let produitTableau = JSON.parse(localStorage.getItem('produit')) || [];
+//         const selectVernis = document.getElementById('vernis').value;
+
+//         const produitIndex = produitTableau.findIndex(p => p._id === produitData._id && p.teinte === selectVernis);
+//         if (produitIndex !== -1) {
+//             produitTableau[produitIndex].quantite++;
+//         } else {
+//             const produitAajouter = {
+//                 ...produitData,
+//                 teinte: selectVernis,
+//                 quantite: 1
+//             };
+//             produitTableau.push(produitAajouter);
+//         }
+
+//         localStorage.setItem('produit', JSON.stringify(produitTableau));
+//         console.log('Produit ajouté ou quantité mise à jour dans le panier');
+//     });
+// };
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    let produitData = {};
 
+    const fetchProduit = async () => {
+        const produitId = window.location.search.split("=")[1];
+        try {
+            const response = await fetch(`http://localhost:3000/api/furniture/${produitId}`);
+            produitData = await response.json();
+            console.log("Produit récupéré:", produitData);
+            produitDisplay();
+        } catch (error) {
+            console.error('Erreur lors de la récupération du produit:', error);
+        }
+    };
 
+    const produitDisplay = async () => {
+        document.getElementById('blocCard').innerHTML = `
+            <div id="card${produitData._id}" class="flex-column-around card-size-produit card">
+                <h3 class="titre-card titre-size">${produitData.name.toUpperCase()}</h3>
+                <img class="image-size" src="${produitData.imageUrl}" alt="image de meuble ${produitData.name}">
+                <div class="bouton-chene">CHÊNE</div>
+                <p class="description-meuble">${produitData.description}</p>
+                <div>
+                    <p class="flex-centre">${(produitData.price / 100).toFixed(2)} Euro</p>
+                    <div class="flex-around div-list-deroulante">
+                        <label for="vernis">Choisir le vernis:</label>
+                        <select name="vernis" id="vernis"></select>
+                    </div>
+                </div>
+                <button id="acheter${produitData._id}" class="bouton-details bouton-acheter">Acheter</button>
+            </div>
+        `;
 
-let produitData = []; // Cette déclaration devrait être un objet {} puisque vous attendez un produit unique
-
-const fetchProduit = async () => {
-    const produitId = window.location.search.split("=")[1];
-    try {
-        const response = await fetch(`http://localhost:3000/api/furniture/${produitId}`);
-        const data = await response.json();
-        console.log(data); // Affiche les données récupérées dans la console
-        produitData = data; // Affectation des données à produitData
-    } catch (error) {
-        console.error('Erreur lors de la récupération du produit:', error);
-    }
-};
-
-const produitDisplay = async () => {
-    await fetchProduit();
-    if (Object.keys(produitData).length === 0) {
-        console.error('Aucun produit trouvé');
-        return; // Sort de la fonction si produitData est vide
-    }
-   
-    document.getElementById('blocCard').innerHTML = `
-    <div id="card${produitData._id}" class="flex-column-around card-size-produit card">
-    <h3 class="titre-card titre-size">${produitData.name.toUpperCase()}</h3>
-    <img class="image-size" src="${produitData.imageUrl}" alt="image de meuble ${produitData.name}">
-    <div class="bouton-chene">CHÊNE</div>
-    <p class="description-meuble">${produitData.description}</p>
-    <div>
-    <p class="flex-centre"> ${produitData.price.toString().replace(/0+$/,"")} Euro</p>
-    
-    <div class="flex-around div-list-deroulante">
-    <label for="vernis">Choisir le vernis:</label>
-    <select name="vernis" id="vernis"></select>
-    </div>
-    </div>
-    <button id="${produitData._id}" class="bouton-details bouton-acheter">Acheter</button>
-    </div>
-    `;
-
-    let select = document.getElementById('vernis');
-    console.log(select);
-
-    console.log(produitData.varnish);
-    produitData.varnish.forEach((vernis) => {
-        document.createElement('option');
-        console.log(vernis);
-        let tagOption = document.createElement('option');
-
-        tagOption.innerHTML = `${vernis}`;
-        tagOption.value = `${vernis}`;
-
-        select.appendChild(tagOption);
-        console.log(tagOption);
-
-    })
-    addBasket(produitData);
-}
-
-produitDisplay();
-
-
-const addBasket = () => {
-    let bouton = document.getElementById(produitData._id);
-    bouton.addEventListener('click', () => {
-        let produitTableau = JSON.parse(localStorage.getItem('produit')) || [];
-        let selectVernis = document.getElementById('vernis').value;
-        
-        // Création de l'objet à ajouter au panier avec teinte et quantité
-        let produitAajouter = Object.assign({}, produitData, {
-            teinte: selectVernis,
-            quantite: 1
+        const select = document.getElementById('vernis');
+        produitData.varnish.forEach(vernis => {
+            const tagOption = document.createElement('option');
+            tagOption.textContent = vernis;
+            tagOption.value = vernis;
+            select.appendChild(tagOption);
         });
-        
-        console.log('Objet à ajouter:', produitAajouter);
 
-        produitTableau.push(produitAajouter);
-        localStorage.setItem('produit', JSON.stringify(produitTableau));
+        addBasket();
+        updateCartQuantity(); // Mise à jour initiale de la quantité dans le panier
+    };
 
-        console.log('Produit ajouté au panier:', produitAajouter);
-    });
-};
+    const addBasket = () => {
+        const bouton = document.getElementById(`acheter${produitData._id}`);
+        bouton.addEventListener('click', () => {
+            let produitTableau = JSON.parse(localStorage.getItem('produit')) || [];
+            const selectVernis = document.getElementById('vernis').value;
+
+            const produitIndex = produitTableau.findIndex(p => p._id === produitData._id && p.teinte === selectVernis);
+            if (produitIndex !== -1) {
+                produitTableau[produitIndex].quantite++;
+            } else {
+                const produitAajouter = {
+                    ...produitData,
+                    teinte: selectVernis,
+                    quantite: 1
+                };
+                produitTableau.push(produitAajouter);
+            }
+
+            localStorage.setItem('produit', JSON.stringify(produitTableau));
+            console.log('Produit ajouté ou quantité mise à jour dans le panier');
+            updateCartQuantity(); // Mise à jour de la quantité après ajout au panier
+        });
+    };
+
+    // Fonction pour mettre à jour la quantité affichée à côté du chariot
+    function updateCartQuantity() {
+        const cartQuantityEl = document.getElementById
